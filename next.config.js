41db1ec4ebedc641f165fcfe34311c70956042d5
Aniket Agarwal <aniketagarwal57@gmail.com>
2022-03-18 12:54:57 +0530

    added database connectivity

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  env: {
+    MONGO_URI:
+      "mongodb+srv://sharkbowl:sharkbowl@cluster0.tu0vw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+};
