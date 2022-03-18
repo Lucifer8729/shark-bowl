7a808c75703c4abaf568363a6bbe04ef3e53aa3e
Shubhankita <63542702+Shubhankita@users.noreply.github.com>
2022-03-18 16:24:58 +0530

    homepage updated

--- components/HomePageTab/Tab1.js
+++ components/HomePageTab/Tab1.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import { Grid } from "@mui/material";
+import { Grid,Typography } from "@mui/material";
 
 const Tab1 = () => {
 	return (
@@ -10,8 +10,9 @@ const Tab1 = () => {
 			direction="row"
 			justifyContent="space-around"
 			alignItems="flex-start"
+			spacing={6}
 		>
-			<Grid item>hello</Grid>
+			<Grid item><img  src="/hello.png"  width={'489'} height={'419'} alt="metor-pic"/></Grid>
 			<Grid item>
 				{
 					//     <Image
@@ -24,13 +25,77 @@ const Tab1 = () => {
 				<Grid
 					container
 					direction="column"
-					justifyContent="center"
-					alignItems="center"
+					//justifyContent="center"
+					//alignItems="center"
 				>
-					<Grid item>Join</Grid>
-					<Grid item>Explore</Grid>
-					<Grid item>Connect</Grid>
-					<Grid item>Grow</Grid>
+					<Grid item>
+					<Grid container  >
+					<Grid item  md={4} >
+					<img  src="/join.png"   alt="metor-pic"/>
+					</Grid>
+					<Grid item  md={4}>
+					<Typography pb={1} component="h1" variant="h5" sx={{ fontWeight: 'bold', color:"white" }}>
+                        Join
+                    </Typography>
+					<Typography pb={1} mt={1} paragraph="true" align="justify" sx={{ color:"white" }}>
+					Set up a profile on SharkBowl to share who you are and show mentors what you need help with.
+					</Typography>
+					</Grid>
+						</Grid>
+					
+					</Grid>
+					<Grid item>
+					<Grid container  >
+					<Grid item  md={4} >
+					<img  src="/explore.png"   alt="metor-pic"/>
+					</Grid>
+					<Grid item  md={4}>
+					<Typography pb={1} component="h1" variant="h5" sx={{ fontWeight: 'bold', color:"white" }}>
+                        Explore
+                    </Typography>
+					<Typography pb={1} mt={1} paragraph="true" align="justify" sx={{ color:"white" }}>
+					Read profiles of mentors who have the experience, background, and knowledge you're looking for.
+					</Typography>
+					</Grid>
+						</Grid>
+					</Grid>
+
+					<Grid item>
+					<Grid container  >
+					<Grid item  md={4} >
+					<img  src="/connect.png"   alt="metor-pic"/>
+					</Grid>
+					<Grid item  md={4}>
+					<Typography pb={1} component="h1" variant="h5" sx={{ fontWeight: 'bold', color:"white" }}>
+                        Connect
+                    </Typography>
+					<Typography pb={1} mt={1} paragraph="true" align="justify" sx={{ color:"white" }}>
+					Reach out to potential mentors by sending them a message. Start a conversation.
+					</Typography>
+					</Grid>
+						</Grid>
+					</Grid>
+					
+					<Grid item>
+					<Grid container  >
+					<Grid item  md={4} >
+					<img  src="/grow.png"   alt="metor-pic"/>
+					</Grid>
+					<Grid item  md={4}>
+					<Typography pb={1} component="h1" variant="h5" sx={{ fontWeight: 'bold', color:"white" }}>
+                        Grow
+                    </Typography>
+					
+					<Typography pb={1} mt={1} paragraph="true" align="justify" sx={{ color:"white" }}>
+					Now you’ve found your mentor. Ask questions. Share. Listen. 
+					</Typography>
+					</Grid>
+						</Grid>
+					</Grid>
+
+					
+
+					
 				</Grid>
 			</Grid>
 		</Grid>
